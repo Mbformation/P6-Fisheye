babe016ebaeb4ddea5a2f264314c3f539c04af36
Mbformation <immvp@hotmail.fr>
2023-11-27 16:00:21 +0100

    CSS Styling: added heart fill on toggle click

--- scripts/components/PortfolioCard.js
+++ scripts/components/PortfolioCard.js
@@ -13,7 +13,9 @@ class PortfolioCard {
     this.btn.setAttribute("aria-label", "Likes");
     this.btn.setAttribute("role", "button");
     this.btn.setAttribute("tabindex", "0");
-    this.btn.innerHTML += `<svg class="heart" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>`;
+    this.btn.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" viewBox="-1 -3 26 25">
+    <path d="M12 21.35l-1.45-1.32C4.53 14.75 0 11.58 0 7.77 0 4.46 2.69 2 6 2c1.93 0 3.68.99 4.71 2.5C12.32 2.99 14.07 2 16 2c3.31 0 6 2.46 6 5.77 0 3.81-4.53 6.98-10.55 12.26L12 21.35z"/>
+  </svg>`;
     this.total = document.createElement("span");
     this.total.classList.add("total");
     this.total.textContent = `${this.item.likes}`;
